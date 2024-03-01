285e6d73c7d22c8a75af517bdd027f54df1fb253
Philani Ncube <ncbphi001@gmail.com>
2024-03-01 13:14:37 +0200

    Refactor CourtsPage component to fetch and display courts data

--- app/dashboard/courts/page.tsx
+++ app/dashboard/courts/page.tsx
@@ -1,4 +1,14 @@
-const CourtsPage = () => {
-  return <div>CourtsPage</div>;
+import { getCourts } from "@/lib/fetchers/courts-fetcher";
+
+const CourtsPage =async  () => {
+
+  const {courts, error} = await getCourts();
+
+  return <div>
+    <h1>Courts</h1>
+    <span>{courts &&
+    <pre>{JSON.stringify(courts, null, 2)}</pre>
+    }</span>
+  </div>;
 };
 export default CourtsPage;
---
+++ lib/fetchers/courts-fetcher.ts
@@ -0,0 +1,35 @@
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
+
+export async function getCourts() {
+  const supabase = createClient();
+
+  try {
+    const { data, error } = await supabase.from("courts").select("*");
+    if (error) {
+      return {
+        error: error.message,
+        courts: data
+      };
+    }
+
+    return {
+    error: null,
+    courts: data
+  }
+
+  } catch (err) {
+    console.error(err);
+    return {
+      error: err,
+      courts: null
+    }
+  } finally {
+    revalidatePath("/dasboard/courts")
+  }
+
+
+   }
+
+
+
