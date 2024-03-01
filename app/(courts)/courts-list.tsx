8db949267c2bdcc90b5d483c5c4279d5df54b226
Philani Ncube <ncbphi001@gmail.com>
2024-03-01 21:06:56 +0200

    Add Link component and update address handling in CourtsList

--- app/(courts)/courts-list.tsx
+++ app/(courts)/courts-list.tsx
@@ -3,6 +3,7 @@
 import { ScrollBar, ScrollArea } from "@/components/ui/scroll-area";
 import { Database } from "@/schema";
 import { GoogleMapsEmbed } from "@next/third-parties/google";
+import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
 import { use, useState } from "react";
 import { set } from "zod";
@@ -15,10 +16,8 @@ type CourtsListProps = {
 const CourtsList = ({ courts }: CourtsListProps) => {
 
   const searchParams = useSearchParams();
-  const [addressState, setAddressState] = useState(
-			"223 2nd Street, Wynberg, Sandton, South Africa",
-		);
-  const router = useRouter()
+  const address = searchParams.get("address") || "South Africa";
+
 
 	return (
 		<div className="flex flex-row space-x-2 h-[calc(100vh-120px)]">
@@ -26,33 +25,33 @@ const CourtsList = ({ courts }: CourtsListProps) => {
 				<ScrollArea className="w-full h-[calc(100vh-150px)] overflow-y-auto">
 					<ul className="flex flex-col gap-y-4">
 						{courts.map((court) => (
-							<div
+							<Link
+               href={{
+                pathname: "/dashboard/courts",
+                query: {
+                  address: court.street_address
+                },
+               }}
 								className="flex flex-col p-3 space-y-2 transition-all duration-300 ease-in-out border-2 rounded-md cursor-pointer border-slate-300 odd:bg-slate-200 hover:bg-slate-300"
 								key={court.id}
-								onClick={() => {
-								setAddressState(court.street_address || "");
-								}}
-								onKeyDown={() => {
-									setAddressState(court.street_address || "");
-								}}
 							>
 								<h3>District {court.district}</h3>
 								<p>Address: {court.street_address}</p>
 								<p className="text-lg font-medium"> {court.court_type} Court</p>
-							</div>
+							</Link>
 						))}
 					</ul>
 					<ScrollBar />
 				</ScrollArea>
 			</div>
-			<div className="flex-1 w-full h-[750px]">
+			<div className="flex-1 w-full h-[760px]">
 				<GoogleMapsEmbed
 					apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}
-					height={750}
+					height={760}
 					width="100%"
 					mode="place"
 					// maptype="satellite"
-					q={addressState}
+					q={address}
 				/>
 			</div>
 		</div>
