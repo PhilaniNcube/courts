6eb69acd6c11560d083be68573166f58b57627bb
Philani Ncube <ncbphi001@gmail.com>
2024-03-31 22:29:24 +0200

    Add separator component to SherrifsPage

--- app/dashboard/sherrifs/page.tsx
+++ app/dashboard/sherrifs/page.tsx
@@ -1,10 +1,12 @@
 import AddSherrif from "@/app/(sherrifs)/add-sherrif";
 import SherrifSummaryTable from "@/app/(sherrifs)/sherrif-summary-table";
+import { Separator } from "@/components/ui/separator";
 
 const SherrifsPage = () => {
   return (
 			<div className="">
 				<AddSherrif />
+        <Separator className="my-4" />
 				<SherrifSummaryTable />
 			</div>
 		);
