a3d18f971c42226d6a6ece3ef9591189a1604523
Philani Ncube <ncbphi001@gmail.com>
2024-03-31 11:06:54 +0200

    Add Google Maps API script to CourtsPage and DashboardPage

--- app/dashboard/courts/page.tsx
+++ app/dashboard/courts/page.tsx
@@ -1,17 +1,21 @@
 import CourtsList from "@/app/(courts)/courts-list";
 import { getCourts } from "@/lib/fetchers/courts-fetcher";
+import Script from "next/script";
 
 const CourtsPage =async  () => {
 
   const {courts, error} = await getCourts();
 
-  return <div>
-    <h1>Courts</h1>
-    <div>
-      {courts ?
-      <CourtsList courts={courts} /> : null
-     }
-    </div>
-  </div>;
+  return (
+			<div>
+				<Script
+          strategy="beforeInteractive"
+					type="text/javascript"
+					src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
+				/>
+				<h1>Courts</h1>
+				<div>{courts ? <CourtsList courts={courts} /> : null}</div>
+			</div>
+		);
 };
 export default CourtsPage;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,14 @@
+import Script from "next/script";
+
 const page = () => {
-  return <div>Dashboard</div>;
+  return (
+			<div>
+				<Script
+					type="text/javascript"
+					src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
+				/>
+        <h1>Dashboard</h1>
+			</div>
+		);
 };
 export default page;
